6393b1376e796bc0ee116539b6817e77326465c6
Abdelrahman Rabah <a.rabah@bbi.ai>
2026-03-12 17:31:25 +0200

    ya rab

--- backend/project_config/settings.py
+++ backend/project_config/settings.py
@@ -77,6 +77,7 @@ ROOT_URLCONF = 'project_config.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://amethis-webpage.netlify.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -84,6 +85,7 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://amethis-webpage.netlify.app",
 ]
 
 SESSION_COOKIE_SAMESITE = None  # Allow cross-port session sharing
